3ba7294c60d8ce51cf369b2bf6c45ec0eb6b97b8
MacLLL <luyangdi1992@gmail.com>
2018-01-19 11:22:21 -0500

    add LSHTableHashChain.scala, which define the abstract class LSHTableHashChains, the hash funtions. the compute abstract method which is used to calculate the hash value of the certain hash functions.

---
+++ src/main/scala/mclab/lsh/hashFamilies/LSHTableHashChain.scala
@@ -0,0 +1,24 @@
+package mclab.lsh.hashFamilies
+
+import mclab.lsh.vector.SparseVector
+
+
+/**
+ * the class implementing the functions chaining in one of the hash tables
+ * @param chainLength the number of hash functions
+ * @param chainedHashFunctions the parameter setup for one set of the functions
+ * @tparam T type of parameter set
+ */
+private[lsh] abstract class LSHTableHashChain[+T <: LSHFunctionParameterSet](
+    private[lsh] val chainLength: Int,
+    private[lsh] val chainedHashFunctions: List[T]) extends Serializable {
+
+  /**
+   * calculate the index of the vector in the hash table corresponding to the set of functions 
+   * defined in this class
+   * @param vector the vector to be indexed
+   * @return the index of the vector
+   */
+  def compute(vector: SparseVector): Int
+  
+}
